64159766217732234a59aa1d15e11efb3ae145ea
truong-magnus <122264583+TruongCNSE161225@users.noreply.github.com>
2023-03-06 15:04:22 +0700

    update password dao testng 1.1

--- src/main/java/org/testng/swt301/Swt301Testng.java
+++ src/main/java/org/testng/swt301/Swt301Testng.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.testng.util.DBHelper;
 
 /**
  *
@@ -76,4 +77,6 @@ public class Swt301Testng {
         }
         return result;
     }
+   
+    
 }
